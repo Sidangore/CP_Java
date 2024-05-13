7dcd3a1f52c3e9abbb2d35900df30c35ceb5d670
F82T59J <siddhant.angore@fiserv.com>
2024-05-13 09:40:03 +0530

    LeetCode 189 update

--- src/main/java/org/siddhantangore/leetcode/Question_189.java
+++ src/main/java/org/siddhantangore/leetcode/Question_189.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class Question_189 {
     public static void main(String[] args) {
-        int[] nums = {1, 2, 3, 4, 5, 6, 7};
+        int[] nums = {1, 2, 3, 4, 5, 6, 7}; // {1, 2, 3, 4, 5, 6, 7}; // {-1, -100, 3, 99}
         int k = 3;
 
         System.out.println(Arrays.toString(nums));
@@ -14,8 +14,9 @@ public class Question_189 {
 
     public static void rotateArrayByK(int[] nums, int k) {
         // Need to incorporate the logic of reversing an array
-        reverseArray(nums, 0, k - 1);
-        reverseArray(nums, k, nums.length - 1);
+        int n = nums.length;
+        reverseArray(nums, n - k, n - 1);
+        reverseArray(nums, 0, n - k - 1);
         reverseArray(nums, 0, nums.length - 1);
     }
 
